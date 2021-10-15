e98e61e4cb7b3771739f92b2ee09675b4a3f7672
Dean-SF <85375794+Dean-SF@users.noreply.github.com>
2021-10-15 10:04:48 -0600

    Un par de clases

---
+++ Codigo/include/Servicios.h
@@ -0,0 +1,16 @@
+#ifndef SERVICIOS_H
+#define SERVICIOS_H
+
+
+class Servicios
+{
+    public:
+        Servicios();
+        virtual ~Servicios();
+
+    protected:
+
+    private:
+};
+
+#endif // SERVICIOS_H
---
+++ Codigo/include/Ventana.h
@@ -0,0 +1,16 @@
+#ifndef VENTANA_H
+#define VENTANA_H
+
+
+class Ventana
+{
+    public:
+        Ventana();
+        virtual ~Ventana();
+
+    protected:
+
+    private:
+};
+
+#endif // VENTANA_H
