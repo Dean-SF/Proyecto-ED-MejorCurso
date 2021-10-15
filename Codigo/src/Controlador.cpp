e4a0a7ed37d8ebe014dd7ba97a81e97b55880dc2
EstebanPP <81255764+EstebanPP@users.noreply.github.com>
2021-10-15 17:27:39 -0600

    ahhhh

--- Codigo/src/Controlador.cpp
+++ Codigo/src/Controlador.cpp
@@ -12,18 +12,35 @@ Controlador::~Controlador(){
     delete servicios;
 }
 
-string Controlador::estadoColas(){}
+string Controlador::estadoColas(){
+    string texto = "";
+    for(int i = 0; i<ventanas->getSize(); i++){
+        texto += ventanas->getPos(i)->getNombre();
+    }
+}
+
 bool Controlador::agregarTiquete(bool preferencial, string codigo){}
+
 string Controlador::atender(string codigoVentana, string numVentanilla){}
+
 bool Controlador::agregarVentana(string codigo, string descripcion, int cantidad){}
+
 bool Controlador::eliminarVentana(string codigo){}
+
 bool Controlador::agregarServicio(){}
+
 bool Controlador::eliminarServicio(){}
+
 bool Controlador::moverServicio(){}
+
 time_t Controlador::tiempoPromedio(){}
+
 string Controlador::dispensadosVentana(){}
+
 string Controlador::atendidosVentana(){}
+
 string Controlador::dispensadosServicio(){}
+
 int Controlador::dispensadosPreferenciales(){
     return totalPreferenciales;
 }
