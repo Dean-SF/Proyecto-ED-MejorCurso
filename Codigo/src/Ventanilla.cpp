f8c6b334c20a75550f4fc6cb71daf2d5a34026c1
EstebanPP <81255764+EstebanPP@users.noreply.github.com>
2021-10-19 14:00:10 -0600

    Mas comentarios

--- Codigo/src/Ventanilla.cpp
+++ Codigo/src/Ventanilla.cpp
@@ -6,34 +6,43 @@ Ventanilla::Ventanilla() {
     atendiendo = nullptr; // Inicialmente la ventanilla no atiende a nadie
 }
 
+//Destructor default de ventanilla limpia el puntero de atendiendo
 Ventanilla::~Ventanilla() {
     delete atendiendo; // limpiar el puntero
 }
 
 // Setter y getters:
+//get del puntero a atendiendo
 Tiquete *Ventanilla::getAtendiendo() {
     return atendiendo;
 }
 
+//set del puntero de atendiendo
 void Ventanilla::setAtendiendo(Tiquete &atentiendo) {
     this->atendiendo = atendiendo;
 }
 
+//get del numero de la ventanilla
 int Ventanilla::getNumero(){
     return numero;
 }
 
+//set del numero de la ventanilla
 void Ventanilla::setNumero(int numero) {
     this->numero = numero;
 }
 
+//get de la cantdad de atendidos
 int Ventanilla::getAtendidos() {
     return atendidos;
 }
 
+//Sobrecarga del operador de comperacion igual
 bool Ventanilla::operator==(const Ventanilla &other) {
     return this->numero == other.numero;
 }
+
+//sobrecarga del operador de comparacion diferente
 bool Ventanilla::operator!=(const Ventanilla &other) {
     return this->numero != other.numero;
 }
